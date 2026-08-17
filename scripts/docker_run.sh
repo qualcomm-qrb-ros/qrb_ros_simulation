92fe77ec6ecf5240d1349b707ff586c1b871758a
Weijie Shen <weijshen@qti.qualcomm.com>
2026-08-17 19:14:15 +0800

    feat: access the container desktop over noVNC instead of SSH
    Serve an XFCE4 desktop through TigerVNC and noVNC so the Gazebo and RViz
    GUIs can be used from a browser, removing the need for an X server and an
    SSH client on the host.
    
    Signed-off-by: Weijie Shen <weijshen@qti.qualcomm.com>

--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -6,7 +6,14 @@
 docker_name="qrb_ros_simulation"
 docker_tag="latest"
 
+# override the default VNC password by exporting VNC_PASSWORD before running
+# this script, e.g. `VNC_PASSWORD=mypassword ./scripts/docker_run.sh`
+vnc_password="${VNC_PASSWORD:-qrbrossim}"
+
+echo "Open http://<host_ip>:6080/vnc.html in your browser to access the desktop (password: ${vnc_password})"
+
 docker run -it --rm \
   --name ${docker_name}_container \
   --network host \
+  -e VNC_PASSWORD="${vnc_password}" \
 	${docker_name}:${docker_tag}
